cfc99a6a22751a15b31ccb247cd5f0cfeda09698
kemboy-254 <kipkemboymichael05@gmail.com>
2021-07-10 23:28:58 +0300

    Update condition_loops1.py

--- condition_loops1.py
+++ condition_loops1.py
@@ -1,10 +1,14 @@
+#Prints all the rest except apple
+
 fruits=["apple","banana","cherry"]
 for x in fruits:
 	if x =="apple":
 		continue
 	print (x)
-	
+
 print('\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n')
+
+#Only prints apple from the list
 
 for x in fruits:
 	if x =="apple":
