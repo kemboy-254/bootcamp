9f497a36b1facb2bedcd72f4a5d3589ad4ac6b9d
kemboy-254 <kipkemboymichael05@gmail.com>
2021-07-10 23:27:04 +0300

    Update condition_loops1.py

--- condition_loops1.py
+++ condition_loops1.py
@@ -1,5 +1,12 @@
 fruits=["apple","banana","cherry"]
 for x in fruits:
 	if x =="apple":
+		continue
+	print (x)
+	
+print('\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n')
+
+for x in fruits:
+	if x =="apple":
 		break
 print (x)
