4b936258d559750cb400dae4e5a9abb1b96aeca4
kemboy-254 <87383570+mike-037@users.noreply.github.com>
2021-07-15 23:16:34 +0300

    Create numbers.py

---
+++ codes/numbers.py
@@ -0,0 +1,9 @@
+num1 =int(input('Please enter the first number:'))
+num2 =int(input('Please enter the second number:'))
+num3 =int(input('Please enter the third number:'))
+if num1 num2 num3 != int:
+	print('Please enter an integer')
+elif num1==num2==num3:
+	print('Equal')
+else:
+	print('Not equal')
