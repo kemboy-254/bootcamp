4ba799150b845c616c446bf22e375d9708ddce42
kemboy-254 <87383570+mike-037@users.noreply.github.com>
2021-07-15 23:12:26 +0300

    Delete helloworld.py

--- helloworld.py
+++
@@ -1,6 +0,0 @@
-first_name =str(input('Please enter the first name: '))
-second_name =str(input('Please enter the second name: '))
-third_name =str(input('Please enter the third name: '))
-
-
-print ("Hi", third_name,",",second_name, "and", first_name )
