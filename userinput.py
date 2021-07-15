96e6d2564011574f7b49ab964cea34891a5ae775
kemboy-254 <87383570+mike-037@users.noreply.github.com>
2021-07-15 23:13:07 +0300

    Delete userinput.py

--- userinput.py
+++
@@ -1,9 +0,0 @@
-num =int(input('Please enter your mark: \n'))
-if num>=80 and num<=100:
-    print('You got an A')
-elif num>60 and num <=79:
-    print('You got a B')
-elif num>40 and num <=59:
-    print('You got a C')
-else: 
-    print('You got Reffered')
