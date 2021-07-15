1aa4cde2c883117284b81cb8984bc671951edc95
kemboy-254 <87383570+mike-037@users.noreply.github.com>
2021-07-15 23:13:19 +0300

    Create userinput.py

---
+++ codes/userinput.py
@@ -0,0 +1,9 @@
+num =int(input('Please enter your mark: \n'))
+if num>=80 and num<=100:
+    print('You got an A')
+elif num>60 and num <=79:
+    print('You got a B')
+elif num>40 and num <=59:
+    print('You got a C')
+else: 
+    print('You got Reffered')
