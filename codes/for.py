debc788a2a8e6928fee2df8cfed9b903fdbd1775
kemboy-254 <kipkemboymichael05@gmail.com>
2021-07-20 18:05:23 +0300

    Update for.py

--- codes/for.py
+++ codes/for.py
@@ -1,7 +1,14 @@
-a = create1D(10, 0)  
-for i in range(10):  
-   a[i] = 9 - i  
-for i in range(10):  
-   a[i] = a[a[i]]  
-for v in a:  
-   writeln(v)
+class Dog:
+  species = "Canis Familiaris"
+  def __init__(self, name, age, color):
+    self.name = name
+    self.age = age
+    self.color = color
+    
+buddy = Dog("Buddy", 9, "Brown")
+miles = Dog("Miles", 4, "White")
+
+
+print(buddy.name,"is",buddy.age,"years old and he is",buddy.color,"in color")
+print(miles.name,"is",miles.age,"years old and he is",miles.color,"in color")
+print("Both",buddy.name,"and",miles.name,"belong to the",Dog.species,"species")
