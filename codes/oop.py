4c51b11683c461df11e09ac2943a6db7892e50cc
kemboy-254 <kipkemboymichael05@gmail.com>
2021-07-20 18:05:13 +0300

    Create oop.py

---
+++ codes/oop.py
@@ -0,0 +1,7 @@
+class Dog:
+    species = "Canis familiaris"  # Class attribute
+
+    def __init__(self, name, age):  #class properties
+        #defining the attributes if the class properties
+        self.name = name
+        self.age = age
