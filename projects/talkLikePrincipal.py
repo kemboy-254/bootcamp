e8f5d62ddef4675297ff800fae87654f48a41049
kemboy-254 <kipkemboymichael05@gmail.com>
2021-07-24 09:28:13 +0300

    Update talkLikePrincipal.py

--- projects/talkLikePrincipal.py
+++ projects/talkLikePrincipal.py
@@ -1,7 +1,7 @@
-print("====PRINCIPLE STORY====")
+print("="*15,"The Principal's Speech",+"="*15)
 
 def nounWord():
-    print("enter nouns")
+    print("Enter nouns: ")
     nounWords=[]
 
     for n in range(5):
@@ -12,7 +12,7 @@ def nounWord():
 
 
 def verbword():
-    print("enter verbs")
+    print("Enter verbs: ")
     verbwords = []
 
     for n in range(5):
@@ -22,7 +22,7 @@ def verbword():
     return verbwords
 
 def adjword():
-    print("enter adjectives")
+    print("Enter adjectives: ")
     adjwords = []
 
     for n in range(5):
@@ -38,6 +38,6 @@ adjwords1 = adjword()
 
 
 
-principlestory="Good morning"+nounwords1[0]+".I would like to "+verbwords1[2]+"all"+adjwords1[3]+"prefects"+"for the"+adjwords1[1]+"work done.Make sure you"+verbwords1[2]+"hard to get good results.Have a"+adjwords1[4]+nounwords1[3]
+principlestory="How are you "+nounwords1[0]+". I would like to "+verbwords1[1]+" all "+adjwords1[2]+" prefects "+"for the "+adjwords1[3]+" job done. Make sure you "+verbwords1[2]+" hard to get good results. Have a "+adjwords1[4] +nounwords1[3]
 
 print(principlestory)
